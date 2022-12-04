974348a67acf4bd91de7b83923d8f89a07cabb8b
Leo Su <50029233+Bpleo@users.noreply.github.com>
2022-12-04 17:09:00 -0500

    Update client.py

--- Client/client.py
+++ Client/client.py
@@ -16,17 +16,27 @@ def upload():
     if request.method == 'POST':
         img = request.files['img']
         img.save(secure_filename(img.filename))
-        url = "http://140.254.14.107:5000/predict"
+        url1 = "http://140.254.14.107:5000/predict"
+        url2 = "http://140.254.14.110:5000/predict"
+        url3 = "http://140.254.14.111:5000/predict"
         image = {'img':open(secure_filename(img.filename),'rb')}
+        
         start = time.time()
-        r1 = requests.post(url, files=image)
-        total_time1 = " Total time: {:.3f}s".format(time.time() - start)
+        r1 = requests.post(url1, files=image)
+        total_time1 = " VGG16 Total time: {:.3f}s".format(time.time() - start)
+        
         start = time.time()
-        r2 = requests.post(url, files=image)
-        total_time2 = " Total time: {:.3f}s".format(time.time() - start)
+        r2 = requests.post(url2, files=image)
+        total_time2 = " RESNET18 Total time: {:.3f}s".format(time.time() - start)
+        
+        start = time.time()
+        r3 = requests.post(url3, files=image)
+        total_time3 = " REGNETx8 Total time: {:.3f}s".format(time.time() - start)
+        
         print(total_time1)
         print(total_time2)
-        return render_template('output.html', value1 = r1.text + total_time1, value2 = r2.text + total_time2)
+        print(total_time3
+        return render_template('output.html', value1 = r1.text + total_time1, value2 = r2.text + total_time2, value3 = r3.text + total_time3)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
